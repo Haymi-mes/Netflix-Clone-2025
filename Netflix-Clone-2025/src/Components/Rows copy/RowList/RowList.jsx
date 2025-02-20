2cae60f85ab0170bc1218034aad4f176ab32beed
Haymi-mes <hewitderessa@gmail.com>
2025-02-20 12:19:00 -0800

    Rows

---
+++ Netflix-Clone-2025/src/Components/Rows copy/RowList/RowList.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Row from "../Row/Row";
+import requests from "../../../utils/requests";
+
+function RowList() {
+  return (
+    <>
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow={true}
+      />
+      <Row title="TRENDING NOW" fetchUrl={requests.fetchTrending} />
+      <Row
+        title="Top Rated Movies"
+        fetchUrl={requests.fetchTopRatedMovies}
+      />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+      <Row title="Tv Show" fetchUrl={requests.fetchTvShow} />
+    </>
+  );
+}
+
+export default RowList;
+
+
