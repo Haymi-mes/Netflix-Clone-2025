dc3acc5a803eb3aead83ce9ff9e6e8456314f9af
Haymi-mes <hewitderessa@gmail.com>
2025-02-20 20:18:34 -0800

    deploy

--- Netflix-Clone-2025/src/Pages/Home.jsx
+++ Netflix-Clone-2025/src/Pages/Home.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import Footer from '../Components/Footer/Footer'
 import Header from '../Components/Header/Header'
 import Banner from '../Components/Banner/Banner'
+import RowList from '../Components/Rows copy/RowList/RowList'
+
 
 const Home = () => {
   return (
    <>
   <Header />
   <Banner />
+  <RowList />
    <Footer />
    </>
   )
