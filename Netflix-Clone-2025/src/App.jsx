cb8af9a7d69178e2c58dcb9ff8644a93a6c186d3
Haymi-mes <hewitderessa@gmail.com>
2025-06-18 14:07:25 -0700

    edit

--- Netflix-Clone-2025/src/App.jsx
+++ Netflix-Clone-2025/src/App.jsx
@@ -1,21 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Home from './Pages/Home'
+import { useState } from "react";
+
+import "./App.css";
+import Home from "./Pages/Home";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-    <div className='App'>
-<Home />
-    </div>
-
-
+      <div className="App">
+        <Home />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
